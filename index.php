9ed0ef7ed847de9bc4bec9db9e40f4cf8748cf8d
ld041298 <39049109+ld041298@users.noreply.github.com>
2021-11-27 19:45:17 +0700

    Add files via upload
    Form login

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<?php>
+	require_once 'init.php';
+	$page = 'Trang chu';
+?>
+<?php include 'header.php'; ?>
+<?php if ($_SESSION['user']):?>
+Chào bạn <?php echo $_SESSION['user']['displayName'] ?> đã quay lại
+<?php else: ?>
+Chào mừng quý khách đến với web
+<?php endif ?>
+<?php include 'footer.php';?>
