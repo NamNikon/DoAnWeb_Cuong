1ed33805af64c1d3eb30d7ff89e2b42e053ea0c3
Nam Nikon <trungnam47.mc@gmail.com>
2021-11-27 23:39:35 +0700

    setting index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php>
+﻿<?php
 	require_once 'init.php';
 	$page = 'Trang chu';
 ?>
@@ -6,6 +6,6 @@
 <?php if ($_SESSION['user']):?>
 Chào bạn <?php echo $_SESSION['user']['displayName'] ?> đã quay lại
 <?php else: ?>
-Chào mừng quý khách đến với web
+CHÀO MỪNG QUÝ KHÁCH ĐẾN CỬA HÀNG
 <?php endif ?>
 <?php include 'footer.php';?>
