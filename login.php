40f84bd862b7371dbf240913a41505a69db06cb0
ld041298 <39049109+ld041298@users.noreply.github.com>
2021-11-29 11:04:51 +0700

    UPDATE CSS
    CSS (Edit Login)

--- login.php
+++ login.php
@@ -1,4 +1,12 @@
-﻿<?php
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>External Style Sheet</title>
+    <link rel="stylesheet" href="Style.css" type="text/css"/>
+</head>
+<?php
 	require_once 'init.php';
 	$page='Đăng nhập';
         var_dump($_POST);
@@ -12,17 +20,21 @@
        
     }
 ?>
-<?php include 'header.php'; ?>
-<h1>Đăng nhập</h1>
-<form method="POST">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email </label>
-    <input type="email" class="form-control" id="Email" name="email" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="password1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="Password" name="password">
-  </div>
-  <button type="submit" class="btn btn-primary">Đăng nhập </button>
-</form>
-<?php include 'footer.php'; ?>
+
+<body >
+   <?php include 'header.php'; ?>
+   <h1 style="color:Tomato;">Đăng nhập</h1>
+   <form method="POST">
+     <div class="mb-3" style="color:DodgerBlue;">
+       <label for="exampleInputEmail1" class="form-label" >Email </label>
+       <input type="email" class="form-control" id="Email" name="email" aria-describedby="emailHelp">
+     </div>
+     <div class="mb-3" style="color:DodgerBlue;">
+       <label for="password1" class="form-label">Password</label>
+       <input type="password" class="form-control" id="Password" name="password">
+     </div>
+     <button type="submit" class="btn btn-primary">Đăng nhập </button>
+   </form>
+   <?php include 'footer.php'; ?>
+</body>
+</html>
